c1144e39359c3d062f95df4388d1f9e449ff88de
Roldham02 <147873099+Roldham02@users.noreply.github.com>
2025-03-22 21:54:43 -0500

    Update map.js

--- map.js
+++ map.js
@@ -67,7 +67,7 @@ function calculateAndDisplayRoute(start, end) {
     }
 
     const osrRouter = new L.Routing.OpenRouteService(API_KEY, {
-        profile : "driving",
+        profile : "driving-hgv",
         timeout: 60000,
         options: {
             language: "en"
